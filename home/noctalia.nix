514d5ecdcd516421ae1e50035b2e7d429d18a735
shin01221 <mohamed@madin372.com>
2026-08-12 10:14:56 +0300

    added obsidian template

--- home/noctalia.nix
+++ home/noctalia.nix
@@ -11,19 +11,13 @@ let
   noctaliaConfig = pkgs.writeText "noctalia-config" (
     readFile ../config/noctalia/noctalia-config.toml
   );
-
-  userTemplates = pkgs.writeText "user-templates" (
-    readFile ../config/noctalia/user-templates.toml
-  );
 in
 {
   xdg.configFile."noctalia/palettes".source = ../config/noctalia/palettes;
   xdg.configFile."noctalia/templates".source = ../config/noctalia/templates;
+  xdg.configFile."noctalia/user-templates.toml".source = ../config/noctalia/user-templates.toml;
 
-  home.activation.ensureNoctaliaConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] (
-    ''
-      ${hashUpdate "noctalia/noctalia-config.toml" noctaliaConfig}
-      ${hashUpdate "noctalia/user-templates.toml" userTemplates}
-    ''
-  );
+  home.activation.ensureNoctaliaConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] (''
+    ${hashUpdate "noctalia/noctalia-config.toml" noctaliaConfig}
+  '');
 }
