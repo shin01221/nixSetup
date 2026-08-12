9826f97a90796b1e8f275d698efdba97e8f307a3
shin01221 <mohamed@madin372.com>
2026-08-12 13:05:36 +0300

    .

--- modules/nixvim/config/plugins/lsp/devenv.nix
+++ modules/nixvim/config/plugins/lsp/devenv.nix
@@ -6,14 +6,24 @@
         or vim.fn.getcwd()
       local bin = root .. '/.devenv/profile/bin/' .. name
       if vim.fn.executable(bin) ~= 1 then
-        vim.notify(name .. ' not found in devenv profile (run: devenv shell)', vim.log.levels.WARN)
+        vim.notify(name .. ' not found in devenv profile', vim.log.levels.WARN)
         return
       end
-      vim.lsp.start({ name = name, cmd = { bin }, root_dir = root })
+
+      local function start()
+        vim.lsp.start({ name = name, cmd = { bin }, root_dir = root })
+      end
+
+      start()
+      vim.defer_fn(function()
+        if #vim.lsp.get_clients({ name = name, bufnr = 0 }) == 0 then
+          vim.lsp.start({ name = name, cmd = { bin, '--stdio' }, root_dir = root })
+        end
+      end, 1500)
     end
 
     vim.api.nvim_create_user_command('DevenvLsp', function(a)
       devenv_lsp(a.args)
-    end, { nargs = 1, complete = 'file' })
+    end, { nargs = 1 })
   '';
 }
